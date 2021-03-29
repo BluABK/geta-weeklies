eb654a1b346c00adec47ce03082c1ca7b17b8003
BluABK <bluabk@bluabk.net>
2021-03-29 13:21:17 +0200

    Started work on MVC conversion.
    Changed div style to be .calc-div class style and moved body html into updateView() function.

--- week 03/task03_mvc_calc.js
+++ week 03/task03_mvc_calc.js
@@ -1,5 +1,34 @@
-var resultBox = document.getElementById('resultBox');
+// Model
+let resultBoxInnerHTML = "";
 
+// View
+function updateView() {
+    document.getElementById("app").innerHTML = `
+    <div id="resultBox" class="calc-div resultBox" id="screen"></div>
+
+    <div class="calc-div firstInRow" onclick="addCharacter(7)">7</div>
+    <div class="calc-div" onclick="addCharacter(8)">8</div>
+    <div class="calc-div" onclick="addCharacter(9)">9</div>
+    <div class="calc-div" onclick="addCharacter('÷')">÷</div>
+
+    <div class="calc-div firstInRow" onclick="addCharacter(4)">4</div>
+    <div class="calc-div" onclick="addCharacter(5)">5</div>
+    <div class="calc-div" onclick="addCharacter(6)">6</div>
+    <div class="calc-div" onclick="addCharacter('×')">×</div>
+
+    <div class="calc-div firstInRow" onclick="addCharacter(1)">1</div>
+    <div class="calc-div" onclick="addCharacter(2)">2</div>
+    <div class="calc-div" onclick="addCharacter(3)">3</div>
+    <div class="calc-div" onclick="addCharacter('-')">-</div>
+
+    <div class="calc-div firstInRow" onclick="addCharacter(0)">0</div>
+    <div class="calc-div" onclick="clearResult();">C</div>
+    <div class="calc-div" onclick="calculate()">=</div>
+    <div class="calc-div" onclick="addCharacter('+')">+</div>
+    `;
+}
+
+// Controller
 function addCharacter(x) {
     resultBox.innerHTML += x;
 }
@@ -14,3 +43,5 @@ function calculate() {
 function clearResult() {
     resultBox.innerHTML = '';
 }
+
+updateView();
