708ce62ad08107d9d4d80d8ac195e93d0e62f2fa
BluABK <bluabk@bluabk.net>
2021-04-09 12:52:08 +0200

    Wrote tests for when other chars than the first one are upper case and refactored fixText to pass all the tests.

--- week 04/Task 01 - Text Formatting/lib.js
+++ week 04/Task 01 - Text Formatting/lib.js
@@ -2,8 +2,8 @@ function fixText(txt) {
     // Trim whitespace.
     let fixedText = txt.trim();
 
-    // Upper case first letter.
-    fixedText = fixedText.charAt(0).toUpperCase() + fixedText.slice(1);
+    // Upper case first letter, lower case all the rest.
+    fixedText = fixedText.charAt(0).toUpperCase() + fixedText.slice(1).toLowerCase();
 
     return fixedText;
 }
--- week 04/Task 01 - Text Formatting/tests.js
+++ week 04/Task 01 - Text Formatting/tests.js
@@ -21,3 +21,15 @@ QUnit.test("fix - ' terje '", function (assert) {
     const expected = 'Terje';
     assert.equal(actual, expected);
 });
+
+QUnit.test("fix - ' tErJe '", function (assert) {
+    const actual = fixText(' tErJe ');
+    const expected = 'Terje';
+    assert.equal(actual, expected);
+});
+
+QUnit.test("fix - 'TERJE '", function (assert) {
+    const actual = fixText(' TERJE ');
+    const expected = 'Terje';
+    assert.equal(actual, expected);
+});
