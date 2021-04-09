922dd7a6c6a80e620a619d979d0c6015146798d7
BluABK <bluabk@bluabk.net>
2021-04-09 13:31:29 +0200

    Implemented isEmailAddr and made it pass all tests.

--- week 04/Task 02 - E-Mail Recognition/lib.js
+++ week 04/Task 02 - E-Mail Recognition/lib.js
@@ -1,3 +1,13 @@
 function isEmailAddr(s) {
-    return false;
+    let hasAt = false;
+    let hasLeadingDot = false;
+    let hasTrailingDot = false;
+
+    if (s.includes('@')) {
+        hasAt = true; 
+        if (s.slice(0,s.indexOf('@')).includes('.')) hasLeadingDot = true;
+        if (s.slice(s.indexOf('@')).includes('.')) hasTrailingDot = true;
+    }
+
+    return hasAt && hasLeadingDot && hasTrailingDot;
 }
