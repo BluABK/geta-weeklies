061b584f57c5efc35328caeab1e20e25215263b2
BluABK <bluabk@bluabk.net>
2021-04-09 12:48:09 +0200

    Implmented fixText so that it passed all the tests.

--- week 04/Task 01 - Text Formatting/lib.js
+++ week 04/Task 01 - Text Formatting/lib.js
@@ -1,3 +1,9 @@
-function fiText(txt) {
-    
+function fixText(txt) {
+    // Trim whitespace.
+    let fixedText = txt.trim();
+
+    // Upper case first letter.
+    fixedText = fixedText.charAt(0).toUpperCase() + fixedText.slice(1);
+
+    return fixedText;
 }
